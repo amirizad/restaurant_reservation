2187b9007c5b578d78f555cce111abf145e1e7b3
Amir Izadoanah <info@amirizad.com>
2017-06-15 20:06:57 -0700

    version 1

---
+++ server.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+var reservations = [];
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/api/tables", function(req, res) {
+  return res.json(reservations);
+});
+
+app.get("/api/waitlist", function(req, res) {
+
+});
+
+app.post("/api/tables", function(req, res) {
+  console.log(req.body);
+  var newreserve = req.body;
+  newreserve.routeName = newreserve.customerName.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newreserve);
+
+  reservations.push(newreserve);
+
+  res.json(newreserve);
+});
+
+app.post("/api/clear", function(req, res) {
+  reservations = [];
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
